4afacd9712e7e518b8c295c1a030c6fe26948fea
Edwardwong0826 <edwardwong_94@outlook.com>
2021-06-05 23:36:24 +0800

    RabbitMQ manually connect test info

--- src/main/java/com/test/rabbitMQ/main.java
+++ src/main/java/com/test/rabbitMQ/main.java
@@ -2,6 +2,7 @@ package com.test.rabbitMQ;
 
 public class main
 {
+    // if is manually connect rabbitMQ by ConnectionFactory, need to start the consumer one by one to get the provider message by type
     public static void main(String[] args)
     {
 
